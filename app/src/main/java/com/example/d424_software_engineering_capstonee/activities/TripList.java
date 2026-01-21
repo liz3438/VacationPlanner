5a8dea432ae121624d266b09f22a269d504cd0d4
liz3438 <49770538+liz3438@users.noreply.github.com>
2026-01-20 20:56:51 -0600

    Packages added

--- app/src/main/java/com/example/d424_software_engineering_capstonee/activities/TripList.java
+++ app/src/main/java/com/example/d424_software_engineering_capstonee/activities/TripList.java
@@ -1,4 +1,4 @@
-package com.example.d424_software_engineering_capstonee;
+package com.example.d424_software_engineering_capstonee.activities;
 
 import android.os.Bundle;
 
@@ -8,13 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.d424_software_engineering_capstonee.R;
+
+public class TripList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_trip_list);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
